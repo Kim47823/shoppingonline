067d60488897f940d8c7af691859e3935d6c8471
Kim47823 <124344744+Kim47823@users.noreply.github.com>
2024-07-16 10:40:37 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'lanlee.mw36oiv.mongodb.net',
+    DB_USER: 'LanLee',
+    DB_PASS: 'Tamlun123',
+    DB_DATABASE: 'shoppingonline',
+    JWT_SECRET: '207ct47823',
+    JWT_EXPIRES: '360000000000000', // in milliseconds
+    EMAIL_USER: 'lan.207ct47823@gmail.com', // gmail service
+    EMAIL_PASS: 'bemc dcvv emwg xlne'
+  };
+  module.exports = MyConstants;
